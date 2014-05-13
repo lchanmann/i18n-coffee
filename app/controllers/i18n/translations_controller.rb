61974f42e0c1bd178067897c058a1afdc8bb17da
Mann <chanmannlim@gmail.com>
2014-05-13 17:31:33 +0700

    small refactor

--- app/controllers/i18n/translations_controller.rb
+++ app/controllers/i18n/translations_controller.rb
@@ -1,7 +1,6 @@
 class I18n::TranslationsController < ApplicationController
   # GET /translations
   def index
-    js_root = Rails.configuration.i18n_js_root
-    render json: { js_root => I18n.t(js_root) }
+    render json: I18n::Coffee.translations
   end
 end
--- lib/i18n-coffee.rb
+++ lib/i18n-coffee.rb
@@ -1,7 +1,21 @@
 module I18n
   module Coffee
+    DEFAULT_TRANSLATIONS_ROOT = :javascripts
+
+    # Rails engine
     class Engine < Rails::Engine
-      config.i18n_js_root = Rails.configuration.i18n_js_root || 'javascripts'
+      config.before_configuration do
+        config.i18n_js_root = I18n::Coffee::DEFAULT_TRANSLATIONS_ROOT
+      end
+    end
+
+    # Translation
+    def self.translations
+      { translations_root => I18n.t(translations_root) }
+    end
+
+    def self.translations_root
+      Rails.configuration.i18n_js_root
     end
   end
 end
