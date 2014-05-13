5c6adff156cfa9383fb4a8fa93510d53befee8d3
Mann <chanmannlim@gmail.com>
2014-05-13 16:12:32 +0700

    configurable i18n js_root

--- app/controllers/i18n/translations_controller.rb
+++ app/controllers/i18n/translations_controller.rb
@@ -1,6 +1,7 @@
 class I18n::TranslationsController < ApplicationController
   # GET /translations
   def index
-    render json: { javascripts: I18n.t('javascripts') }
+    js_root = Rails.configuration.i18n_js_root
+    render json: { js_root => I18n.t(js_root) }
   end
 end
--- lib/i18n-coffee.rb
+++ lib/i18n-coffee.rb
@@ -1,6 +1,7 @@
 module I18n
   module Coffee
     class Engine < Rails::Engine
+      config.i18n_js_root = Rails.configuration.i18n_js_root || 'javascripts'
     end
   end
 end
